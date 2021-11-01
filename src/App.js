3799c7cb6194e511a401f5f817bc9aede8c2a9d0
Liana van Zyl <Lianavz@Lianas-MacBook-Pro.local>
2021-11-01 21:59:06 +0100

    Finished app for Meal planning and calories intake but needs fixing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import MealList from "./MealList";
 
 function App() {
+  const [mealData, setMealData] = useState(null);
+  const [calories, setCalories] = useState(2000);
+
+  function getMealData() {
+    {/*axios
+      .get('https://api.spoonacular.com/recipes/716429/information?apiKey=618396b0abe143398becafd2108f3164&timeFrame=week&targetCalories${calories}')
+      .then(resp => {
+        setMealData(data);
+        console.log(resp.data);
+      });*/}
+    fetch(
+      `https://api.spoonacular.com/mealplanner/generate?timeFrame=day&targetCalories=${calories}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setMealData(data);
+      })
+      .catch(() => {
+        console.log("error");
+      });
+  }
+
+  function handleChange(e) {
+    setCalories(e.target.value);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    < div className="App" >
+      <section className="controls">
+        <input
+          type="number"
+          placeholder="Calories (e.g 2000)"
+          onChange={handleChange} />
+        <button onClick={getMealData}>Get Your Weekly Meal Plan</button>
+        {mealData && <MealList mealData={mealData} />}
+      </section>
+    </div >
+
   );
-}
+};
 
 export default App;
